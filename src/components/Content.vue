3303b5d75be15f6b682dc5cff30871f6ab31baea
Cee Nell <cnell@usgs.gov>
2024-02-20 09:27:11 -0800

    website template

---
+++ src/components/Content.vue
@@ -0,0 +1,69 @@
+ <template>
+  <div class='container'>
+    
+  </div>
+</template>
+
+  <script>
+
+
+  import { onMounted, ref, reactive } from "vue";
+  import * as d3 from 'd3';
+
+  export default {
+    setup() {
+     
+      const publicPath = import.meta.env.BASE_URL;
+
+      onMounted(() => {
+          loadData();
+          const pageHeight = window.innerHeight;
+
+      });
+      function loadData() {
+        /* let promises = [
+        d3.csv(publicPath + ), 
+      ];*/
+      Promise.all(promises).then(callback)
+      }
+      function callback(data) {
+          /* keyMessages = data[0]; */
+      }
+
+    return {
+          /* populate with return items */
+    };
+  }
+}
+
+  </script>
+  
+  
+  <style>
+
+.container {
+  width: 100%;
+  height: 100vh;
+  background: transparent;
+  overflow: hidden
+}
+  
+.text-container {
+    width: 100%; /* Full width on mobile */
+    max-width: 500px; /* Maximum width of the textbox */
+    padding: 10px;
+  /*  border: 1px solid #ccc; */
+    border-radius: 4px;
+    margin: 20px;
+}
+/* Styles for larger screens */
+@media (min-width: 600px) {
+    .text-container {
+        margin: 0 auto; /* Center the container */
+        margin-top: 25px;
+        max-width: 1000px; /* Maximum width of the container */
+    }
+  }
+  
+  </style>
+  
---
+++ src/views/VisualizationView.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div class="parent-container">
+   <Wavy />
+  </div>
+  </section>
+</template>
+
+<script setup>
+  import Wavy from '../components/Content.vue';
+</script>
+
+<style scoped>
+.section-header {
+  color: var(--dodger-blue);
+}
+.parent-container {
+  width: 100vw; /* Set the width to 100% of the viewport width */
+  height: 100vh; /* Set the height to 100% of the viewport height */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
+</style>
