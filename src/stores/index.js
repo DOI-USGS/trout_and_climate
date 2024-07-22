63a0a71db71a52eaae7913bed9ed001a1dbd6479
Cee <cnell@usgs.gov>
2024-07-22 00:32:50 -0500

    add outro nav

--- src/stores/index.js
+++ src/stores/index.js
@@ -6,23 +6,24 @@ export const store = reactive({
   hotWater: content.hotWater,
   warmWater: content.warmWater,
   coldWater: content.coldWater,
+  outro: content.outro || [],
   allChapters: [],
   currentType: 'intro',
   currentIndex: 0,
   selectedOptions: [],
   lastIntroIndex: content.intro.length - 1,
-  
+
   initializeAllChapters() {
     this.allChapters = [...this.intro];
   },
   
   addChapters(type) {
-    const newChapters = this[type].map((chapter, index) => {
+    const newChapters = this[type]?.map((chapter, index) => {
       return { ...chapter, globalIndex: this.allChapters.length + index };
-    });
+    }) || [];
     this.allChapters.push(...newChapters);
   },
-  
+
   setTypeAndIndex(newType, newIndex) {
     this.currentType = newType;
     this.currentIndex = newIndex;
@@ -30,24 +31,42 @@ export const store = reactive({
       this.addChapters(newType);
     }
   },
-  
+
   nextChapter() {
     if (this.currentIndex < this.allChapters.length - 1) {
       this.currentIndex++;
     } else if (this.currentType !== 'intro') {
-      this.resetToIntro();
+      if (this.allOptionsChosen()) {
+        this.navigateToOutro();
+      } else {
+        this.resetToIntro();
+      }
     }
   },
-  
+
   prevChapter() {
     if (this.currentIndex > 0) {
       this.currentIndex--;
     }
   },
-  
+
   resetToIntro() {
     this.currentType = 'intro';
     this.currentIndex = this.lastIntroIndex;
+  },
+
+  allOptionsChosen() {
+    return this.selectedOptions.length === 3;
+  },
+
+  navigateToOutro() {
+    const outroStartIndex = this.allChapters.length;
+    const outroChapters = this.outro.map((chapter, index) => {
+      return { ...chapter, globalIndex: outroStartIndex + index };
+    });
+    this.allChapters.push(...outroChapters);
+    this.currentType = 'outro';
+    this.currentIndex = outroStartIndex;
   }
 });
 
