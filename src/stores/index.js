14fefc7ff6d383db0fcd18c2138f220a35f2b3e0
Cee <cnell@usgs.gov>
2024-07-21 23:41:59 -0500

    dynamically handle chapter navigation to build

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,30 +1,55 @@
-// store/index.js
 import { reactive } from 'vue';
 import content from '@/assets/content.json';
 
 export const store = reactive({
-  chapters: content.intro,
+  intro: content.intro,
+  hotWater: content.hotWater,
+  warmWater: content.warmWater,
+  coldWater: content.coldWater,
+  allChapters: [],
   currentType: 'intro',
   currentIndex: 0,
   selectedOptions: [],
+  lastIntroIndex: content.intro.length - 1,
+  
+  initializeAllChapters() {
+    this.allChapters = [...this.intro];
+  },
+  
+  addChapters(type) {
+    const newChapters = this[type].map((chapter, index) => {
+      return { ...chapter, globalIndex: this.allChapters.length + index };
+    });
+    this.allChapters.push(...newChapters);
+  },
+  
   setTypeAndIndex(newType, newIndex) {
     this.currentType = newType;
     this.currentIndex = newIndex;
-    this.chapters = content[newType];
-  },
-  nextChapter() {
-    if (this.currentIndex < this.chapters.length - 1) {
-      this.currentIndex++;
+    if (newType !== 'intro') {
+      this.addChapters(newType);
     }
   },
+  
+  nextChapter() {
+    if (this.currentIndex < this.allChapters.length - 1) {
+      this.currentIndex++;
+    } else if (this.currentType !== 'intro') {
+      this.resetToIntro();
+    }
+  },
+  
   prevChapter() {
     if (this.currentIndex > 0) {
       this.currentIndex--;
     }
   },
+  
   resetToIntro() {
     this.currentType = 'intro';
-    this.currentIndex = content.intro.length - 1;
-    this.chapters = content.intro;
+    this.currentIndex = this.lastIntroIndex;
   }
 });
+
+// Initialize allChapters on store load
+store.initializeAllChapters();
