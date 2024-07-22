23029e8a32e41557ad814ea0fae963515715b81b
Cee <cnell@usgs.gov>
2024-07-21 23:30:58 -0500

    return to intro

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,3 +1,4 @@
+// store/index.js
 import { reactive } from 'vue';
 import content from '@/assets/content.json';
 
@@ -5,6 +6,7 @@ export const store = reactive({
   chapters: content.intro,
   currentType: 'intro',
   currentIndex: 0,
+  selectedOptions: [],
   setTypeAndIndex(newType, newIndex) {
     this.currentType = newType;
     this.currentIndex = newIndex;
@@ -19,5 +21,10 @@ export const store = reactive({
     if (this.currentIndex > 0) {
       this.currentIndex--;
     }
+  },
+  resetToIntro() {
+    this.currentType = 'intro';
+    this.currentIndex = content.intro.length - 1;
+    this.chapters = content.intro;
   }
 });
