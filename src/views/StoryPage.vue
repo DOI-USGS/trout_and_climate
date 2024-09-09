783a60a67da232df013e1e33d1fe4fcd0e04def9
Cee <cnell@usgs.gov>
2024-09-08 17:05:24 -0700

    move buttonson mobile

--- src/views/StoryPage.vue
+++ src/views/StoryPage.vue
@@ -4,14 +4,16 @@
     <div id="images-container" >
       <img :src="currentChapter.bknd" :alt="currentChapter.alt || 'Chapter Image'" />
     </div>
-    <div id="chapter-content">
+
+    <!-- Desktop Layout: Buttons on either side of text -->
+    <div v-if="!mobileView" id="chapter-content-desktop">
       <RetroButton
-      id = "prev-button"
-      label="&#8249;"
-      :buttonStyle="prevButtonStyle"
-      @click="prevChapter"
-      :isDisabled="isFirstPage"
-      :style="{ visibility: isFirstPage ? 'hidden' : 'visible' }"
+        id = "prev-button"
+        label="&#8249;"
+        :buttonStyle="prevButtonStyle"
+        @click="prevChapter"
+        :isDisabled="isFirstPage"
+        :style="{ visibility: isFirstPage ? 'hidden' : 'visible' }"
       />
       <div id="chapter-text" v-if="currentChapter">
         <p v-html="currentChapter.text"></p>
@@ -25,7 +27,32 @@
         :style="{ visibility: isLastPage ? 'hidden' : 'visible' }"
       />
     </div>
-  </div>
+
+    <!-- Mobile Layout: Buttons below text -->
+    <div v-else id="chapter-content-mobile">
+      <div id="chapter-text" v-if="currentChapter">
+        <p v-html="currentChapter.text"></p>
+      </div>
+      <div id="button-container-mobile">
+      <RetroButton
+        id = "prev-button"
+        label="&#8249;"
+        :buttonStyle="prevButtonStyle"
+        @click="prevChapter"
+        :isDisabled="isFirstPage"
+        :style="{ visibility: isFirstPage ? 'hidden' : 'visible' }"
+      />
+      <RetroButton
+        id = "next-button"
+        label="&#8250;"
+        :buttonStyle="nextButtonStyle"
+        @click="nextChapter"
+        :isDisabled="isLastPage"
+        :style="{ visibility: isLastPage ? 'hidden' : 'visible' }"
+      />
+      </div>
+    </div>
+    </div>
   <br/>
     <hr class="content-divider" />
     <div id="references-container">
@@ -128,111 +155,110 @@ export default {
 </script>
 
 <style scoped>
+#grid-container-viz {
+display: flex;
+flex-direction: column;
+width: 100%;
+height: 90vh;
+}
+#page-title {
+  margin: 0 auto;
+  text-align: center;
+  line-height: 1.5;
+  padding-bottom: 10px;
+}
+#chapter-content-desktop {
+  display: flex;
+  justify-content: space-between; 
+  align-items: stretch; 
+  height: 100%;
+  flex-grow: 1;
+}
+#chapter-content-mobile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  #grid-container-viz {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 90vh;
-   /*  width: 100%;
-    margin: 0 auto 0 auto;
-    height: 90vh;
-    grid-template-columns: 1fr;
-    column-gap: 0;
-    grid-template-rows: max-content auto 1fr;
-    row-gap: 2vh;
-    grid-template-areas:
-      "title"
-      "image"
-      "chapter-content";
-    @media screen and (max-height: 770px) {
-      grid-template-rows: max-content auto 15vh max-content;
-    }
-    @media screen and (max-width: 600px) {
-      column-gap: 5%;
-      grid-template-rows: max-content auto minmax(18vh, max-content) max-content max-content;
-      grid-template-columns: 1fr 1fr;
-      grid-template-areas: 
-        "title"
-        "image"
-        "chapter-content"
-        "prev next"
-      ;
-    } */
-  }
-  #page-title {
-    margin: 0 auto;
-    text-align: center;
-    line-height:2;
-  }
-  #chapter-content {
-    display: flex; 
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: stretch; /* Ensure items stretch to fill the container height */
-    height: 100%; /* Full height of the container */
-    margin-top: 20px;
-  }
-  #chapter-text {
-    flex-grow: 1; /* Allow the text container to grow and take available space */
-    display: flex;
-    align-items: center;
-    justify-self: center;
-    padding: 0 20px;
-  }
-  #images-container {
-    grid-area: image;
-    position: relative;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    margin-top: 20px;
-
-  }
-  #prev-button, #next-button {
-    font-size: 6rem; /* Large font size for the arrows */
-    background-color: white;
-    border: none;
-    width: 10px; /* Set width for the buttons */
-    min-width: 100px;
-    height: 100%; /* Full height of the chapter-content container */
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-  }
-  #start-button {
-    grid-area: start;
-    align-self: start;
-  }
-  #end-button {
-    grid-area: end;
-    align-self: start;
-  }
-  #references {
-    margin-top: 20px;
-  }
-
+#button-container-mobile {
+  display: flex;
+  width: 100%;
+  justify-content: space-between; 
+}
+#prev-button-mobile, #next-button-mobile {
+  width: 50%;
+  height: auto;
+  font-size: 3rem;
+}
+#chapter-text {
+  flex-grow: 1; 
+  display: flex;
+  align-items: center;
+  justify-self: center;
+  padding: 0 20px;
+}
+#prev-button, #next-button {
+  font-size: 6rem; 
+  background-color: white;
+  border: none;
+  width: 100px; 
+  min-width: 100px;
+  height: 100%; 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+#references {
+  margin-top: 20px;
+}
+#images-container {
+  width: 100%;
+  height: auto;
+  text-align: center;
+  margin-top: 20px;
+}
 #images-container img {
   max-width: 100%;
   max-height: 562.5px;
   object-fit: contain;
-  @media screen and (max-height: 770px) {
-    max-height: 60vh;
-  }
-  @media screen and (max-width: 600px) {
-    height: 30vh;
-  }
 }
-/* Horizontal line between content and references */
+
 .content-divider {
   border: 0;
   height: 1px;
-  background: #ccc; /* Light gray color for the line */
-  margin: 20px 0 ; /* Add space above and below the line */
+  background: #ccc; 
+  margin: 20px 0 ; 
 }
 #authorship-container p {
   font-size:2rem;
   margin-bottom: 50px;
+}
+
+/* Mobile-specific styles */
+@media screen and (max-width: 600px) {
+  #chapter-content {
+    flex-direction: column;
+    align-items: center;
+  }
+  #chapter-title {
+    line-height: 1.2rem;
+  }
+  #chapter-text {
+    width: 100%; 
+    padding: 10px;
+  }
+  #images-container {
+    width: 100%;
+    height: auto;
+    text-align: center;
+    margin-top: 5px;
+  }
+  #button-container-mobile {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+  }
 }
 </style>
